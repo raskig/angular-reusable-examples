5b1a89717a908f59b4c12a0314f34aa1fb007afd
Gabor Raski <gabor.raski@gmail.com>
2017-07-17 16:19:38 +0100

    handleException function added

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -17,40 +17,34 @@ export class PostService {
 
   getPost() {
       return this.http.get(this.url)
-        .catch((error: Response) => {
-          return Observable.throw(new AppError(error));
-        });
+        .catch(this.handleError);
     }
 
   updatePost(post) {
     return this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
-      .catch((error: Response) => {
-        if (error.status === 404) {
-          return Observable.throw(new NotFoundError);
-        }
-        return Observable.throw(new AppError(error));
-      });
+      .catch(this.handleError);
   }
 
   deletePost(postId) {
     return this.http.delete(this.url + '/' + postId)
-      .catch((error: Response) => {
-        if (error.status === 404) {
-          return Observable.throw(new NotFoundError);
-        }
-        return Observable.throw(new AppError(error));
-      });
+      .catch(this.handleError);
 
   }
 
   createPost(post) {
     return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if (error.status === 400) {
-          return Observable.throw(new BadInputError(error.json()));
-        }
-        return Observable.throw(new AppError(error));
-      });
+      .catch(this.handleError);
+  }
+
+
+  private handleError(error: Response) {
+    if (error.status === 404) {
+      return Observable.throw(new NotFoundError);
+    }
+    if (error.status === 400) {
+      return Observable.throw(new BadInputError(error.json()));
+    }
+    return Observable.throw(new AppError(error));
   }
 
 }
